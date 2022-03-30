f12c0fc953cc06d8666d502f6a1f7ba61e37d26b
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-03-30 13:09:04 +0600

    nav update

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Link = (props) => {
     const { name, link } = props.route
     return (
-        <nav>
-            <a className='mr-20 align-right' href={link}> {name}</a>
+        <nav className='bg-slate-900 text-white font-mono py-3'>
+            <a className='mr-20 align-right ' href={link}> {name}</a>
         </nav>
     );
 };
--- src/components/Route/Route.js
+++ src/components/Route/Route.js
@@ -17,12 +17,12 @@ const Route = () => {
 
 
 
-        <nav>
-            <div onClick={() => setOpen(!open)} className='w-6 h-6  md:hidden'>
+        <nav className='bg-slate-900 text-white '>
+            <div onClick={() => setOpen(!open)} className='w-6 h-6 bg-slate-900 text-white md:hidden'>
                 {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
 
             </div>
-            <ul className={`md:flex justify-end md:static m-5 absolute ${open ? 'top-6' : 'top-[-150px]'} `}>
+            <ul className={`md:flex justify-end md:static absolute duration-500 ease-in w-full bg-slate-900 text-white ${open ? 'top-6' : 'top-[-350px]'} `}>
                 {
                     Routes.map(route => <Link key={route.id} route={route}></Link>)
                 }
